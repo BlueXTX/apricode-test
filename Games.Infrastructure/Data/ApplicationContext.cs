2374cc69511f618135d6ba3a1fdacba2ebc0d05d
BlueXTX <bluextchannel@gmail.com>
2022-02-10 20:14:34 +0300

    Configured infrastructure

---
+++ Games.Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using Games.Application;
+using Games.Application.Interfaces;
+using Games.Application.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Games.Infrastructure.Data;
+
+public class ApplicationContext : DbContext, IApplicationContext
+{
+    public DbSet<Game> Games { get; set; }
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    {
+    }
+
+    public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await base.SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DependencyInjection).Assembly);
+    }
+}
---
+++ Games.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Games.Application.Interfaces;
+using Games.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Games.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddGamesInfrastructure(IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationContext>(builder =>
+            builder.UseInMemoryDatabase("Database"));
+        services.AddScoped<IApplicationContext, ApplicationContext>();
+        return services;
+    }
+}
