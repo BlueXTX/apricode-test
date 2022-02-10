a81ed255c2573d8b21b1437eb3fb8c927b029cda
BlueXTX <bluextchannel@gmail.com>
2022-02-10 18:14:43 +0300

    Created automapper profiles

---
+++ Games.Application/Profiles/GameProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Games.Application.Models.Dto;
+using Games.Application.Models.Entities;
+
+namespace Games.Application.Profiles;
+
+public class GameProfile : Profile
+{
+    public GameProfile()
+    {
+        CreateMap<GameCreateDto, Game>()
+            .ForMember(prop => prop.Id,
+                opts => opts.Ignore());
+    }
+}
