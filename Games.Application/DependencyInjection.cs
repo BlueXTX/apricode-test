4e2540523ee2a1824c72f55463757fd04ea2cf2b
BlueXTX <bluextchannel@gmail.com>
2022-02-10 18:19:48 +0300

    Added application di

---
+++ Games.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Games.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddGamesApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        return services;
+    }
+}
