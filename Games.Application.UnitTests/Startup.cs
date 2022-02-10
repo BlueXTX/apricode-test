4a6dd3f1e07b5b48767e4de14573243dbfbedf92
BlueXTX <bluextchannel@gmail.com>
2022-02-10 18:32:15 +0300

    Added di in tests project

---
+++ Games.Application.UnitTests/Startup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Games.Application.UnitTests;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddGamesApplication();
+    }
+}
