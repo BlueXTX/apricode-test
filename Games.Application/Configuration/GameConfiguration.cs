3cb8590e78975d7c08f283f236a1d3b4909830cc
BlueXTX <bluextchannel@gmail.com>
2022-02-10 18:12:40 +0300

    Created ef model configuration

---
+++ Games.Application/Configuration/GameConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using Games.Application.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Games.Application.Configuration;
+
+public class GameConfiguration : IEntityTypeConfiguration<Game>
+{
+    public void Configure(EntityTypeBuilder<Game> builder)
+    {
+        builder
+            .Property(prop => prop.Genres)
+            .HasConversion(
+                v => JsonSerializer.Serialize(v, new JsonSerializerOptions()),
+                v =>
+                    JsonSerializer.Deserialize<IEnumerable<string>>(v,
+                        new JsonSerializerOptions()) ?? Array.Empty<string>());
+    }
+}
