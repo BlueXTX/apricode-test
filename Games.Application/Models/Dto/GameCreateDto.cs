f3b758041aa225da891938c9f9eefc96cf63f9bb
BlueXTX <bluextchannel@gmail.com>
2022-02-10 17:57:57 +0300

    Created data models

---
+++ Games.Application/Models/Dto/GameCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Games.Application.Models.Dto;
+
+public record GameCreateDto
+{
+    public string Name { get; init; } = string.Empty;
+    public string Developer { get; init; } = string.Empty;
+    public IEnumerable<string> Genres { get; init; } = Array.Empty<string>();
+}
---
+++ Games.Application/Models/Entities/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace Games.Application.Models.Entities;
+
+public record Game
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Developer { get; set; } = string.Empty;
+    public IEnumerable<string> Genres { get; set; } = Array.Empty<string>();
+}
